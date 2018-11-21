261817e452a76117f144ff0854ae5a414506b463
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-11-20 19:52:09 -0500

    Function is passing through for a large pizza

--- pizza_price_tdd.rb
+++ pizza_price_tdd.rb
@@ -8,6 +8,10 @@ class Pizza < Minitest::Test
   end
   def test_to_make_one_medium
     size = "medium"
-    assert_equal(10, pizza_price(7, size))
+    assert_equal(14, pizza_price(7, size))
+  end
+  def test_to_make_one_small
+    size = "large"
+    assert_equal(0, pizza_price(3, size))
   end
 end
