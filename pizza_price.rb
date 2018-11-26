7042ff8df5396444dc74625664fe0187b5ea51fc
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-11-26 14:00:23 -0500

    You can now click the text to select pizza ingredients

--- pizza_price.rb
+++ pizza_price.rb
@@ -1,14 +1,18 @@
 def pizza_price(length, array)
   price = 0
+  counter = 0
   array.each do |item|
     if item == "small"
       price += 5
+      counter =+ 1
     elsif item == "medium"
       price += 7
+      counter += 1
     elsif item == "large"
       price += 10
+      counter += 1
     end
   end
-  price += length
+  price += length - counter
   return price
 end
